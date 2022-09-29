864847346fab832d00275a7771291f8e0c5db36c
gyujae <wjdrbwo1206@naver.com>
2022-09-29 21:16:01 +0900

    eslint setting

---
+++ src/components/Layout/Header/Header.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  import { categoryStore } from '../../../stores';
+  import svelteLogo from '../../../assets/svelte.svg';
+  
+  const handleClickMovieCategory = () => {
+    categoryStore.set('movie')
+  }
+  
+  const handleClickMovieTVShow = () => {
+    categoryStore.set('tv')
+  }
+
+  
+</script>
+
+<header class='wrapper'>
+  <a href="https://svelte.dev" target="_blank"> 
+    <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+  </a>
+
+  <ul class='category-list'>
+    <li class='category-list-item' class:selected={$categoryStore === 'movie'}>
+      <button type='button' on:click={handleClickMovieCategory}>
+        Movies
+      </button>
+    </li>
+    <li class='category-list-item' class:selected={$categoryStore === 'tv'}>
+      <button type='button' on:click={handleClickMovieTVShow}>
+        TV Shows
+      </button>
+    </li>
+  </ul>
+</header>
+
+<style>
+  .wrapper {
+    display: flex;
+    align-items: center;
+    background-color: #141414;
+    width: 100%;
+    color: #e5e5e5;
+    position: fixed;
+  }
+
+  .logo {
+    height: 2em;
+    padding: 1.5em;
+  }
+  
+  .category-list {
+    display: flex;
+    column-gap: 10px;
+    font-size: 13px;
+  }
+
+  .category-list-item button {
+    all: unset;
+    cursor: pointer;
+  }
+
+  .category-list-item:hover {
+    opacity: 0.8;
+  }
+  
+  .selected {
+    font-weight: 700;
+  }
+
+</style>
---
+++ src/components/Layout/Layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Header from './Header/Header.svelte';
+</script>
+
+<Header />
+<main>
+  <slot />
+</main>
+
+
+<style>
+  main {
+    min-height: 100vh;
+    background-color: #141414;
+    padding: 80px 20px;
+  }
+</style>
