761e41126a24661077029633127678072ebbe3b9
gyujae <wjdrbwo1206@naver.com>
2022-10-01 17:38:27 +0900

    feat: tvs component

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,34 @@
 <script lang="ts">
   import Layout from './components/Layout/Layout.svelte'
   import Movies from './components/Movies/Movies.svelte';
-  import type { TMovieCategory } from './services';
+  import TVs from './components/TVs/TVs.svelte';
+  import type { TMovieCategory, TvCategory } from './services';
   import { categoryStore } from './stores';
-  categoryStore
-  let categories: TMovieCategory[] = ["top_rated", "popular", "upcoming", "now_playing"]
+  
+  let movieCategories: TMovieCategory[] = ["top_rated", "popular", "upcoming", "now_playing"];
+  let tvCategories: TvCategory[] = ['top_rated' , 'popular' , 'airing_today' , 'on_the_air'];
 </script>
 
 <Layout>
   {#if $categoryStore === 'movie'}
-    <ul class="movies-list">
-      {#each  categories as category}
-        <Movies cateogry={category} />
+    <ul class="media-list">
+      {#each movieCategories as category}
+        <Movies category={category} />
+      {/each}
+    </ul>
+  {/if}
+
+  {#if $categoryStore === 'tv'}
+    <ul class="media-list">
+      {#each tvCategories as category}
+        <TVs cateogry={category} />
       {/each}
     </ul>
   {/if}
 </Layout>
 
 <style>
-  ul.movies-list {
+  ul.media-list {
     display: flex;
     flex-direction: column;
     row-gap: 30px;
--- src/components/Movies/Movies.svelte
+++ src/components/Movies/Movies.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
   import { getMovies, type TMovieCategory } from "../../services";
-  export let cateogry: TMovieCategory = 'now_playing';
+  export let category: TMovieCategory = 'now_playing';
   import Carousel from 'svelte-carousel'
-  let moviesPromise = getMovies(cateogry);
+  let moviesPromise = getMovies(category);
 </script>
 
 {#await moviesPromise then movies}
   <Carousel particlesToShow={6} particlesToScroll={6}>
     {#each movies as movie}
-      <img src={movie.backdrop_path} alt={movie.title} />
+      {#if movie.backdrop_path}
+        <img src={movie.backdrop_path} alt={movie.title} />
+      {/if}
     {/each}
   </Carousel>
 {/await}
